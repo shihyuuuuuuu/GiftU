dd79c62cae7602345f47b605aab6a63617c73a7f
鄭禕仁 <yijen.cheng@junyiacademy.org>
2020-03-09 17:07:14 +0800

    Deployment settings for heroku

--- gift_u/settings.py
+++ gift_u/settings.py
@@ -181,3 +181,5 @@ AUTHENTICATION_BACKENDS = [
         'django.contrib.auth.backends.ModelBackend',
         'allauth.account.auth_backends.AuthenticationBackend',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
