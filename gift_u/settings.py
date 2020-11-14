b5c2dda3c2b5bbbaf586acadeccce593fea3f49d
鄭禕仁 <ivan861118@gmail.com>
2020-11-15 01:07:09 +0800

    add smtp env

--- gift_u/settings.py
+++ gift_u/settings.py
@@ -187,7 +187,7 @@ AUTHENTICATION_BACKENDS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ntustartup2019@gmail.com'
-EMAIL_HOST_PASSWORD = 'osebceyysxigbhff'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
