98c08b4865f15884e868ddc1c3f166c5e69cdc77
鄭禕仁 <ivan861118@gmail.com>
2020-11-15 02:01:47 +0800

    debug

--- gift_u/apps/email_service/tasks.py
+++ gift_u/apps/email_service/tasks.py
@@ -2,6 +2,6 @@ from celery import shared_task
 from django.core.mail import send_mail
 
 @shared_task
-def send_email_task():
-    send_mail('hello','this is a test message, Just Shut up','ntustartup2019@gmail.com',['ivan861118@gmail.com'], fail_silently=False)
+def send_email_task(recipient, title, message):
+    send_mail(title, message,'ntustartup2019@gmail.com',[recipient], fail_silently=False)
     return 'Task Completed!'
--- gift_u/apps/email_service/views.py
+++ gift_u/apps/email_service/views.py
@@ -6,5 +6,9 @@ from django.http import HttpResponse
 from .tasks import send_email_task
 
 def sendmail(request):
-    send_email_task.delay()
-    return HttpResponse('<h1>EMAIL HAS BEEN SENT WITH CELERY!</h1>')
+    if request.method == 'POST':
+        data = request.POST
+        send_email_task.delay(recipient=data['recipient'], title=data['title'], message=data['message'])
+        return HttpResponse('<h1>EMAIL HAS BEEN SENT WITH CELERY!</h1>')
+    else:
+        return render(request, 'email_service/send_mail.html', locals())
