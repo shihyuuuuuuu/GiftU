34c561d4a6154d35a5c32d21406fc06ca4933599
ivan861118 <34654196+ivan861118@users.noreply.github.com>
2020-11-16 00:36:16 +0800

    add email template (#17)

--- gift_u/apps/email_service/tasks.py
+++ gift_u/apps/email_service/tasks.py
@@ -1,7 +1,12 @@
 from celery import shared_task 
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 @shared_task
 def send_email_task(recipient, title, message):
-    send_mail(title, message,'ntustartup2019@gmail.com',[recipient], fail_silently=False)
+    html_message = render_to_string('email_service/email_template.html',  {
+        'title':title,
+        'message':message
+    })
+    send_mail(title, message,'ntustartup2019@gmail.com',[recipient], fail_silently=False, html_message=html_message)
     return 'Task Completed!'
--- gift_u/apps/email_service/views.py
+++ gift_u/apps/email_service/views.py
@@ -5,10 +5,14 @@ from django.http import HttpResponse
 # Create your views here.
 from .tasks import send_email_task
 
-def sendmail(request):
+def index(request):
     if request.method == 'POST':
         data = request.POST
-        send_email_task.delay(recipient=data['recipient'], title=data['title'], message=data['message'])
+        send_email_task(recipient=data['recipient'], title=data['title'], message=data['message'])
         return HttpResponse('<h1>EMAIL HAS BEEN SENT WITH CELERY!</h1>')
     else:
         return render(request, 'email_service/send_mail.html', locals())
+    
+
+
+        
