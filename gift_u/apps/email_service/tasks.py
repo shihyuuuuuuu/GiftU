3cbade0a51739a2a3ae5f9697db963946fc956fe
鄭禕仁 <ivan861118@gmail.com>
2020-11-18 00:47:48 +0800

    can reply anonymous email

--- gift_u/apps/email_service/tasks.py
+++ gift_u/apps/email_service/tasks.py
@@ -1,12 +1,22 @@
 from celery import shared_task 
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from .utils import encode
 
 @shared_task
-def send_email_task(recipient, title, message):
-    html_message = render_to_string('email_service/email_template.html',  {
-        'title':title,
-        'message':message
-    })
+def send_email_task(recipient, title, message, sender=None):
+    if sender:
+        sender = encode(sender) 
+        html_message = render_to_string('email_service/email_template.html',  {
+            'title':title,
+            'message':message,
+            'reply_link':f"https://giftu.herokuapp.com/email_service/?recipient={sender}"
+        })
+    else:
+        html_message = render_to_string('email_service/email_template.html',  {
+            'title':title,
+            'message':message,
+            'reply_link':None
+        })
     send_mail(title, message,'心禮系<ntustartup2019@gmail.com>',[recipient], fail_silently=False, html_message=html_message)
     return 'Task Completed!'
--- gift_u/apps/email_service/views.py
+++ gift_u/apps/email_service/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .utils import decode
+
 
 # Create your views here.
 from .tasks import send_email_task
@@ -8,10 +10,13 @@ from .tasks import send_email_task
 def index(request):
     if request.method == 'POST':
         data = request.POST
-        send_email_task.delay(recipient=data['recipient'], title=data['title'], message=data['message'])
+        recipient = decode(data['recipient']) if request.POST.get('anonymous') else data['recipient']
+
+        send_email_task.delay(sender=data['sender'], recipient=recipient, title=data['title'], message=data['message'])
         return HttpResponse('<h1>感謝您使用本服務，信件已經寄出囉！</h1>')
     else:
-        return render(request, 'email_service/send_mail.html', locals())
+        recipient = request.GET.get("recipient")
+        return render(request, 'email_service/send_mail.html',context={"recipient":recipient})
     
 
 
