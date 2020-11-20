68a3d1dc6d558cc433c3a0c71edeaf9cd7d5a98e
鄭禕仁 <ivan861118@gmail.com>
2020-11-20 23:18:26 +0800

    fix reply mechanism

--- gift_u/apps/email_service/views.py
+++ gift_u/apps/email_service/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .utils import decode
+from .utils import encode, decode
 
 
 # Create your views here.
@@ -11,23 +11,25 @@ def index(request):
     if request.method == 'POST':
         
         data = request.POST
+        print(data)
         # Todo: 用serializer做驗證
-        is_reply = data['is_reply']
+        is_reply = True if data['is_reply'] == 'true' else False
+        if is_reply:
+            sender = decode(data['sender'])
+            recipient = decode(data['recipient'])
 
-        sender = decode(data['sender']) if is_reply == True else data['sender']
-        if sender == '': sender = None #should be moved to serializer logic
+            reply_link = None #回信不能再回覆
+        else:
+            sender = data['sender']
+            if sender == '': sender = None #should be moved to serializer logic
+            recipient = data['recipient']
 
-        recipient = decode(data['recipient']) if is_reply == True else data['recipient']
+            reply_link = f"https://giftu.herokuapp.com/email_service/?sender={encode(sender)}&recipient={encode(recipient)}" if sender else None #寄信時有提供email，才會有回覆連結
         
         sender_info = {
-            sender_name: sender.split('@')[0] if sender else '匿名使用者',
-            anonymous: True if data['anonymous'] == 'true' else False,
-        }
-
-        if is_reply: #回信不能再回覆
-            reply_link = None
-        else:
-            reply_link = f"https://giftu.herokuapp.com/email_service/?sender={encode(sender)}&recipient={encode(sender)}" if sender else None #寄信時有提供email，才會有回覆連結
+            "sender_name": sender.split('@')[0] if sender else '匿名使用者',
+            "anonymous": True if data['anonymous'] == 'true' else False,
+        }   
 
         send_email_task.delay(sender_info=sender_info, recipient=recipient, title=data['title'], message=data['message'], reply_link=reply_link)
         return HttpResponse('<h1>感謝您使用本服務，信件已經寄出囉！</h1>')
